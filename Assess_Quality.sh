36e4aeec86036e51881d5e81c2969280a8054fc5
Paul Hoffman <hoff0792@umn.edu>
2015-11-23 17:45:07 -0600

    New integration method

--- Assess_Quality.sh
+++ Assess_Quality.sh
@@ -12,29 +12,29 @@ set -e
 set -u
 set -o pipefail
 
-CONFIG_FILE=$1
-UTILS=$2
+# CONFIG_FILE=$1
+# UTILS=$2
+#
+# source ${CONFIG_FILE}
+# source ${UTIlS}
 
-source ${CONFIG_FILE}
-source ${UTIlS}
-
-#   Check our sample list
-checkSamples ${RAW_SAMPLES}
-
-if [[ "$?" -eq 1 ]]; then echo "Error with samples"; exit 1; fi
-
-#   Check to see if FastQC was loaded or defined properly
-if ! `command -v fastqc > /dev/null 2> /dev/null`
-then
-    echo "Please make sure FastQC is either loaded or installed properly"
-    exit 1
-fi
-
-if ! `command -v parallel > dev/null 2> /dev/null`
-then
-    echo "Please make sure GNU Parallel is installed properly"
-    exit 1
-fi
+# #   Check our sample list
+# checkSamples ${RAW_SAMPLES}
+#
+# if [[ "$?" -eq 1 ]]; then echo "Error with samples"; exit 1; fi
+#
+# #   Check to see if FastQC was loaded or defined properly
+# if ! `command -v fastqc > /dev/null 2> /dev/null`
+# then
+#     echo "Please make sure FastQC is either loaded or installed properly"
+#     exit 1
+# fi
+#
+# if ! `command -v parallel > dev/null 2> /dev/null`
+# then
+#     echo "Please make sure GNU Parallel is installed properly"
+#     exit 1
+# fi
 
 function Assess_Quality() {
     sampleList="$1"
@@ -48,7 +48,7 @@ function Assess_Quality() {
 
 export -f Assess_Quality
 
-echo "Assess_Quality ${RAW_SAMPLES} ${OUT_DIR} ${PROJECT}" | qsub -l "${QC_QSUB}" -m abe -M "${EMAIL}" -N "${PROJECT}"_Quality_Assessment
+# echo "Assess_Quality ${RAW_SAMPLES} ${OUT_DIR} ${PROJECT}" | qsub -l "${QC_QSUB}" -m abe -M "${EMAIL}" -N "${PROJECT}"_Quality_Assessment
 
 # cat ${RAW_SAMPLES} | parallel "fastqc --outdir ${OUT} {}"
 #
