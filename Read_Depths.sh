5dd84b2ca4b05e6a45272c53c8f398618b8c5058
mojaveazure <hoff0792@umn.edu>
2016-01-14 08:56:02 -0600

    Minor fixes

--- Read_Depths.sh
+++ Read_Depths.sh
@@ -11,15 +11,16 @@ set -o pipefail
 #       read depth, and get rid of unzipped FastQC report files
 function getCoverage() {
     local zipFile="$1" # What is our zipped file?
+    local -i target="$2" # What is our target size?
     local zipDirectory=`basename "${zipFile}" .zip` # Get the basename of the file/name of unzipped directory
     local sampleName=`basename "${zipFile}" _fastqc.zip` # Get the name of the sample
     local ROOT=`dirname "${zipFile}"` # Where is the zipfile stored?
     cd "${ROOT}" # Change to directory where zipfiles are contained
     unzip "${zipFile}" # Unzip the FastQC report directory
     cd "${zipDirectory}" # Change into the report directory
-    local totalSequence=`grep 'Total Sequences' fastqc_data.txt | cut -f 2` # Extract total number of sequences
-    local sequenceLength=`grep 'Sequence length' fastqc_data.txt | cut -f 2 | rev | cut -d '-' -f 1 | rev` # Extract sequence length
-    echo -e "${sampleName} \t $[${totalSequence} * ${sequenceLength} / ${target}]" >> "${outDirectory}"/"${project}"_depths.txt # Do math and write to output file
+    local -i totalSequence=`grep 'Total Sequences' fastqc_data.txt | cut -f 2` # Extract total number of sequences
+    local -i sequenceLength=`grep 'Sequence length' fastqc_data.txt | cut -f 2 | rev | cut -d '-' -f 1 | rev` # Extract sequence length
+    echo -e "${sampleName}\t$(( totalSequence * sequenceLength / target ))" >> "${outDirectory}"/"${project}"_depths.txt # Do math and write to output file
     #   Get rid of unzipped FastQC report files
     cd "${ROOT}"
     rm -rf "${zipDirectory}"
@@ -45,9 +46,9 @@ function Read_Depths() {
     local zipSamples="$2" # Zipped files for getCoverage
     local outDirectory="$3"/Read_Depths # The out directory to store our files
     local project="$4" # Name for the output files
-    local target="$5" # Target size for getCoverage
+    local -i target="$5" # Target size for getCoverage
     mkdir -p "${outDirectory}" # Make our output directory
-    parallel getCoverage {} :::: "${zipSamples}" # Run getCoverage in parallel
+    #parallel getCoverage {} ${target} :::: "${zipSamples}" # Run getCoverage in parallel
     parallel "getCounts {} :::: ${rawSamples}" > "${outDirectory}"/"${project}"_counts.txt # Run getCounts in parallel and write results to an output file
 }
 
