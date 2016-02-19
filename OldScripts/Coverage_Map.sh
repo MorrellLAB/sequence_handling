ba4ae28418ced410f4418f15a1d7d4eaaf2f6ef8
Paul Hoffman <hoff0792@umn.edu>
2016-02-19 15:02:44 -0600

    Work towards enabling coverage mapping

--- OldScripts/Coverage_Map.sh
+++ OldScripts/Coverage_Map.sh
@@ -4,85 +4,68 @@
 #   from BAM files using BEDTools. Please have BEDTools installed
 #   before using this script
 
-#PBS -l mem=12gb,nodes=1:ppn=8,walltime=16:00:00
-#PBS -m abe
-#PBS -M user@example.com
-#PBS -q lab
-
 set -e
-set -u
 set -o pipefail
 
-module load parallel
+#   What are the dependencies for Coverage_Mapping?
+declare -a Coverage_Mapping_Dependencies=(bedtools Rscript)
 
-#   This script is a QSub submission for generating a coverage map for a batch of BAM files.
-#   To use, on line 5, change the 'user@example.com' to your own email address
-#       to get notifications on start and completion for this script
-#   Define a path to the bedtools in the 'BEDTOOLS' field on line 47
-#       If using MSI, leave the definition as is to use their installation of bedtools
-#       Otherwise, it should look like this:
-#           BEDTOOLS=${HOME}/software/bedtools
-#       Please be sure to comment out (put a '#' symbol in front of) the 'module load bedtools' on line 46
-#       And to uncomment (remove the '#' symbol) from lines 47 and 48
-#   Add the full file path to list of samples on the 'SAMPLE_INFO' field on line 51
-#       This should look like:
-#           SAMPLE_INFO=${HOME}/Directory/list.txt
-#       Use ${HOME}, as it is a link that the shell understands as your home directory
-#           and the rest is the full path to the actual list of samples
-#   Name the project in the 'PROJECT' field on line 54
-#       This should look lke:
-#           PROJECT=Genetics
-#   Put the full directory path for the output in the 'SCRATCH' field on line 57
-#       This should look like:
-#           SCRATCH="${HOME}/Out_Directory"
-#       Adjust for your own out directory.
-#   Define a reference annotation file for the coverage mapping process in the 'REF_ANN' field on line 61
-#       This should look like:
-#           REF_ANN=${HOME}/Directory/annotation.bed
-#       This can be either a BED file or a GFF file
-#   Run this script using the qsub command
-#       qsub Coverage_Map.sh
-#   This script outputs a text file for each sample
+#   A function to make our outdirectories
+function makeOutDirectories() {
+    local outPrefix="$1"
+    mkdir -p "${outPrefix}"/CoverageMaps "${outPrefix}"/CoveragePlots
+}
 
-#   Path to BEDTools
-#   On MSI, loading the module works
-#   Otherwise, have path to BEDTools directory
-module load bedtools
-#BEDTOOLS=
-#export PATH=$PATH:/$BEDTOOLS
+#   Export the function
+export -f makeOutDirectories
 
-#   List of BAM files for coverage mapping
-SAMPLE_INFO=
+#   A function to map the coverage
+function mapCoverage() {
+    local BAMFile="$1"
+    local name="$2"
+    local out="$3"
+    local referenceAnnotation="$4"
+    bedtools coverage -hist -abam "${BAMFile}" -b "${referenceAnnotation}" > "${out}"/"${name}".coverage.hist.txt
+}
 
-#   Name of Project
-PROJECT=
+#   Export the function
+export -f mapCoverage
 
-#   Scratch directory for output, 'scratch' is a symlink to individual user scratch at /scratch*
-SCRATCH=
+#   A function to plot the coverage
+function plotCoverage() {
+    local sample="$1" # Figure out what this sample is
+    local out="$2" # Where do we store our output files?
+    local helperScripts="$3" # Where is our helper script located?
+    local name="$(basename $sample .coverage.hist.txt)" # Get the name of the sample
+    rm -f "${out}"/"${name}"_* # Remove any pipes
+    mkfifo "${out}"/"${name}"_genome.PIPE # Make a pipe for the genome map
+    mkfifo "${out}"/"${name}"_exon.PIPE # Make a pipe for the exon map
+    mkfifo "${out}"/"${name}"_gene.PIPE # Make a pipe for the gene map
+    grep 'all' "$sample" > "${out}"/"${name}"_genome.PIPE & # Make a map for the genome
+    grep 'exon' "$sample" > "${out}"/"${name}"_exon.PIPE & # Make a map for exons
+    grep 'gene' "$sample" > "${out}"/"${name}"_gene.PIPE & # Make a map for genes
+    Rscript "${helperScripts}"/plot_cov.R "${out}"/"${name}"_genome.PIPE "${out}"/"${name}"_exon.PIPE "$sample" > "${out}"/"${name}"_gene.PIPE "${out}" "${name}"
+}
 
-#   Annotation file to be considered
-#       This is the reference .bed or .gff file
-REF_ANN=
+#   Export the function
+export -f plotCoverage
 
-#   Other variables, don't need to be user-specified
-DATE=`date +%Y-%m-%d`
-OUT=${SCRATCH}/${PROJECT}/Coverage_Map
-mkdir -p ${OUT}
+function Coverage_Mapping() {
+    local sampleList="$1" # What is our list of samples?
+    local outDirectory="$2"/Coverage_Mapping # Where do we store our results?
+    local referenceAnnotation="$3" # What is our reference annotation file?
+    local helperScripts="$4"/HelperScripts # Where do we keep our helper scripts?
+    # local project="$5" # What do we call our samples?
+    local -a names=($(parallel basename {} .bam :::: "${sampleList}")) # Get a list of names
+    parallel --jobs 2 --xapply mapCoverage {1} {2} "${outDirectory}/CoverageMaps" "${referenceAnnotation}" :::: "${sampleList}" ::: "${sampleNames}" # Generate our coverage maps in text histogram form
+    local -a histograms=($(find ${outDirectory}/CoverageMaps -name "*.coverage.hist.txt" | sort)) # Get a list of our coverage histograms
+    parallel plotCoverage {} "${outDirectory}/CoveragePlots" "${helperScripts}" ::: "${histograms[@]}" # Generate our coverage plots
+    find "${outDirectory}/CoveragePlots" -type p -exec rm {} \; # Remove any excess pipes
+}
 
-#   List of sample names
-for i in `seq $(wc -l < "${SAMPLE_INFO}")`
-do
-    s=`head -"$i" "${SAMPLE_INFO}" | tail -1`
-    basename "$s" .bam >> "${OUT}"/sample_names.txt
-done
+#   Export the function
+export -f Coverage_Mapping
 
-SAMPLES="${OUT}"/sample_names.txt
+# #   Make an output list for use with
+# find ${OUT} -name "*.coverage.hist.txt" | sort > ${OUT}/${PROJECT}_samples_coverage.txt
 
-#   Do the work here
-cd ${OUT}
-parallel --jobs 2 --xapply "bedtools coverage -hist -abam {1} -b ${REF_ANN} > ${OUT}/Sample_{2}_${PROJECT}_${DATE}.coverage.hist.txt" :::: ${SAMPLE_INFO} :::: ${SAMPLES}
-
-#   Make an output list for use with
-find ${OUT} -name "*.coverage.hist.txt" | sort > ${OUT}/${PROJECT}_samples_coverage.txt
-echo "List of samples for  can be found at"
-echo "${OUT}/${PROJECT}_samples_coverage.txt"
