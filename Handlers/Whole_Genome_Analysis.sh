0bf20d0620483b4739e0b9b0ee43aa0b057bb724
Chaochih Liu <liux1299@umn.edu>
2017-04-27 16:44:21 -0500

    draft of WGS analysis handler

---
+++ Handlers/Whole_Genome_Analysis.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#   This script analyzes whole genome sequencing data for 10x Genomics data.
+#   Additional documentation can be found at:
+#       https://support.10xgenomics.com/genome-exome/software/pipelines/latest/using/wgs
+
+set -o pipefail
+
+#   What are the dependencies of Whole_Genome_Analysis?
+declare -a Whole_Genome_Analysis_Dependencies=(longranger java)
+
+#   A function to run Whole Genome Whole_Genome_Analysis
+function WGS_Analysis() {
+    local prefix="$1" # What are our sample prefixes?
+    local fastqDir="$2" # Where is our FASTQ directory?
+    local referenceDir="$3" # Where is our compatible reference?
+    local variantCaller="$4" # What variant caller are we using?
+    local vcf="$5" # Are we using a pre-called VCF file?
+    local sex="$6" # What sex is our organism?
+    local outDir="$7" # Where are we storing our results?
+    local out="${outDir}"/Whole_Genome_Analysis # Full path to output directory
+
+    #   Create out directory name
+    sampleID=`basename ${out}`
+
+    #   Go into output directory
+    #   Long Ranger outputs results in current working directory
+    cd ${outDir}
+    #   Run longranger wgs
+    longranger wgs --id=${sampleID} \
+                   --sample=${prefix} \
+                   --fastqs=${fastqDir} \
+                   --reference=${referenceDir} \
+                   --vcmode=${variantCaller} \
+                   --sex=${sex}
+}
+
+export -f WGS_Analysis
