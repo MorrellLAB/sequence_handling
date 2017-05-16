f1c936eda075b7dce9eba757c9127a4dd632d43c
Skylar Wyant <wyant008@umn.edu>
2017-05-16 13:20:58 -0500

    Clarifications to Read_Depths

--- Handlers/Read_Depths.sh
+++ Handlers/Read_Depths.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-#   This script calculates coverage from FastQC output
-#   Please run the 'Assess_Quality.sh' script before running
-
 set -o pipefail
 
-#   What are the dependencies for Read_Counts?
-declare -a Read_Counts_Dependencies=(parallel)
+#   What are the dependencies for Read_Depths?
+declare -a Read_Depths_Dependencies=(parallel)
 
 #   Define a function to unzip the FastQC report files, extract
 #       total number of sequences and sequence length, calculate
-#       read counts, and get rid of unzipped FastQC report files
+#       read depths, and get rid of unzipped FastQC report files
 function getCoverage() {
     local zipFile="$1" # What is our zipped file?
     local -i target="$2" # What is our target size?
@@ -24,7 +21,7 @@ function getCoverage() {
     cd "${zipDirectory}" # Change into the report directory
     local -i totalSequence=`grep 'Total Sequences' fastqc_data.txt | cut -f 2` # Extract total number of sequences
     local -i sequenceLength=`grep 'Sequence length' fastqc_data.txt | cut -f 2 | rev | cut -d '-' -f 1 | rev` # Extract sequence length
-    echo -e "${sampleName}\t$(( totalSequence * sequenceLength / target ))" >> "${outDirectory}"/"${project}"_counts.txt # Do math and write to output file
+    echo -e "${sampleName}\t$(( totalSequence * sequenceLength / target ))" >> "${outDirectory}"/"${project}"_depths.txt # Do math and write to output file
     #   Get rid of unzipped FastQC report files
     cd "${ROOT}"
     rm -rf "${zipDirectory}"
@@ -33,28 +30,16 @@ function getCoverage() {
 #   Export the coverage function
 export -f getCoverage
 
-#   Define a function to get the read counts
-function getCounts() {
-    local sample="$1" # What is our sample?
-    local name="$( basename ${sample} )" # Get the name of the sample
-    local count="$( bioawk -cfastx 'END{print NR}' $sample )" # Find the read counts
-    printf %s"$name \t $count \n" # Write the results all pretty like
-}
-
-#   Export counts function
-export -f getCounts
-
 #   Define a function to run all of this
-function Read_Counts() {
-    local rawSamples="$1" # FastQ samples for getCounts
+function Read_Depths() {
+    local rawSamples="$1" # FastQ samples for getDepths
     local zipSamples="$2" # Zipped files for getCoverage
-    local outDirectory="$3"/Read_Counts # The out directory to store our files
+    local outDirectory="$3"/Read_Depths # The out directory to store our files
     local project="$4" # Name for the output files
     local -i target="$5" # Target size for getCoverage
     mkdir -p "${outDirectory}" # Make our output directory
     parallel getCoverage {} ${target} ${outDirectory} ${project} :::: "${zipSamples}" # Run getCoverage in parallel
-    #parallel "getCounts {} :::: ${rawSamples}" #> "${outDirectory}"/"${project}"_counts.txt # Run getCounts in parallel and write results to an output file
 }
 
 #   Export the function
-export -f Read_Counts
+export -f Read_Depths
