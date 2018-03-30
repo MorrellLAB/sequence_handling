914b03790c2429bd1c8930bfac34110d02b8c70e
Skylar Wyant <wyant008@umn.edu>
2018-03-30 13:42:17 -0500

    Add option to fix quality scores to Haplotype_Caller Allow all handlers using task arrays to submit arrays with only one sample Attempting to use an output directory that hasn't been created yet no longer throws an error Move RAW_SAMPLES to Adapter_Trimming Change ALTERNATE_SAMPLES to QA_SAMPLES in Quality_Assessment and no longer use RAW_SAMPLES Change Qsub settings for Realigner_Target_Creator and Indel_Realigner

--- Handlers/Haplotype_Caller.sh
+++ Handlers/Haplotype_Caller.sh
@@ -19,10 +19,27 @@ function Haplotype_Caller() {
     local reference="$4" # Where is the reference sequence?
     local heterozygosity="$5" # What is the nucleotide diversity/bp?
     local memory="$6" # How much memory can java use?
+    local qscores="$7" # Do we fix quality scores?
     declare -a sample_array=($(grep -E ".bam" "${sample_list}")) # Turn the list into an array
     local sample="${sample_array[${PBS_ARRAYID}]}" # Which sample are we working on currently?
     local sample_name=$(basename ${sample} .bam) # What is the sample name without the suffix?
     mkdir -p "${out}" # Make sure the out directory exists
+    if [[ "${qscores}" == true ]]
+    then
+    #   Run GATK using the parameters given
+    (set -x; java -Xmx"${memory}" -jar "${gatk}" \
+	    -T HaplotypeCaller \
+	    -R "${reference}" \
+	    -I "${sample}" \
+	    -o "${out}/${sample_name}_RawGLs.g.vcf" \
+	    -nct 4 \
+	    --genotyping_mode DISCOVERY \
+	    --heterozygosity "${heterozygosity}" \
+	    --emitRefConfidence GVCF \
+	    -variant_index_type LINEAR \
+        --fix_misencoded_quality_scores \
+	    -variant_index_parameter 128000)
+    else
     #   Run GATK using the parameters given
     (set -x; java -Xmx"${memory}" -jar "${gatk}" \
 	    -T HaplotypeCaller \
@@ -35,6 +52,7 @@ function Haplotype_Caller() {
 	    --emitRefConfidence GVCF \
 	    -variant_index_type LINEAR \
 	    -variant_index_parameter 128000)
+    fi
 }
 
 #   Export the function
