eeeeadcde60fcf8357b5a1c91c893422df6fee99
Skylar Wyant <wyant008@umn.edu>
2018-07-09 16:37:28 -0500

    Address issue #37 by adding two new parameter options to Haplotype_Caller

--- Handlers/Haplotype_Caller.sh
+++ Handlers/Haplotype_Caller.sh
@@ -20,12 +20,17 @@ function Haplotype_Caller() {
     local heterozygosity="$5" # What is the nucleotide diversity/bp?
     local memory="$6" # How much memory can java use?
     local qscores="$7" # Do we fix quality scores?
+    local trim_active="$8" # Do we trim the active regions?
+    local force_active="$9" # Do we force all bases to be active?
     declare -a sample_array=($(grep -E ".bam" "${sample_list}")) # Turn the list into an array
     local sample="${sample_array[${PBS_ARRAYID}]}" # Which sample are we working on currently?
     local sample_name=$(basename ${sample} .bam) # What is the sample name without the suffix?
     mkdir -p "${out}" # Make sure the out directory exists
-    if [[ "${qscores}" == true ]]
-    then
+    #   Determine the GATK settings based on the parameters set in the Config file
+    local settings='' # Make an empty string
+    if [[ "${qscores}" == true ]]; then settings="--fix_misencoded_quality_scores "; fi
+    if [[ "${trim_active}" == true ]]; then settings="${settings} --dontTrimActiveRegions "; fi
+    if [[ "${trim_active}" == true ]]; then settings="${settings} --forceActive "; fi
     #   Run GATK using the parameters given
     (set -x; java -Xmx"${memory}" -jar "${gatk}" \
 	    -T HaplotypeCaller \
@@ -37,22 +42,8 @@ function Haplotype_Caller() {
 	    --heterozygosity "${heterozygosity}" \
 	    --emitRefConfidence GVCF \
 	    -variant_index_type LINEAR \
-        --fix_misencoded_quality_scores \
-	    -variant_index_parameter 128000)
-    else
-    #   Run GATK using the parameters given
-    (set -x; java -Xmx"${memory}" -jar "${gatk}" \
-	    -T HaplotypeCaller \
-	    -R "${reference}" \
-	    -I "${sample}" \
-	    -o "${out}/${sample_name}_RawGLs.g.vcf" \
-	    -nct 4 \
-	    --genotyping_mode DISCOVERY \
-	    --heterozygosity "${heterozygosity}" \
-	    --emitRefConfidence GVCF \
-	    -variant_index_type LINEAR \
-	    -variant_index_parameter 128000)
-    fi
+	    -variant_index_parameter 128000 \
+        ${settings})
 }
 
 #   Export the function
