45dc92dc22151000ddb6c2c6969bb94f9ee8f984
Skylar Wyant <wyant008@umn.edu>
2018-10-11 16:58:20 -0500

    Add sorting to NP_SAM_Processing Remove unnecessary statistics output from NP_SAM_Processing since NP reads are never paired

--- Handlers/NP_SAM_Processing.sh
+++ Handlers/NP_SAM_Processing.sh
@@ -20,21 +20,21 @@ function NP_SAM_Processing() {
     #   Get the name of the sample without the path in front
     local sampleName=$(basename "${sample}" .sam)
     #   Convert to BAM format
-    (set -x; samtools view -b -T "${reference}" "${sample}" > "${outDirectory}/${sampleName}.bam")
+    (set -x; samtools view -bhT "${reference}" "${sample}" > "${outDirectory}/${sampleName}_unsorted.bam"
+    #   Sort the BAM file
+    samtools sort "${outDirectory}/${sampleName}_unsorted.bam" > "${outDirectory}/${sampleName}.bam"
     #   Run flagstat on the finished file to assess quality
-    (set -x; samtools flagstat "${outDirectory}/${sampleName}.bam" > "${outDirectory}/Statistics/Finished_BAM_Stats/${sampleName}_stats.txt")
+    samtools flagstat "${outDirectory}/${sampleName}.bam" > "${outDirectory}/Statistics/Finished_BAM_Stats/${sampleName}_stats.txt")
     #   Add the data from flagstat to the summary file
     local num_reads=$(head -n 1 "${outDirectory}/Statistics/Finished_BAM_Stats/${sampleName}_stats.txt" | cut -f 1 -d " ")
     local percent_mapped=$(grep "%" "${outDirectory}/Statistics/Finished_BAM_Stats/${sampleName}_stats.txt" | head -n 1 | cut -f 2 -d "(" | cut -f 1 -d " ")
-    local percent_paired=$(grep "%" "${outDirectory}/Statistics/Finished_BAM_Stats/${sampleName}_stats.txt" | head -n 2 | tail -n 1 | cut -f 2 -d "(" | cut -f 1 -d " ")
-    local percent_singleton=$(grep "%" "${outDirectory}/Statistics/Finished_BAM_Stats/${sampleName}_stats.txt" | tail -n 1 | cut -f 2 -d "(" | cut -f 1 -d " ")
-    local num_split_chr=$(tail -n 2 "${outDirectory}/Statistics/Finished_BAM_Stats/${sampleName}_stats.txt" | head -n 1 | cut -f 1 -d " ")
-    local percent_split_chr=$(echo "${num_split_chr}/${num_reads}" | bc -l)
-    echo -e "${sampleName}\t${num_reads}\t${percent_mapped}\t${percent_paired}\t${percent_singleton}\t${percent_split_chr}" >> "${outDirectory}/Statistics/${project}_mapping_summary.txt"
+    echo -e "${sampleName}\t${num_reads}\t${percent_mapped}" >> "${outDirectory}/Statistics/${project}_mapping_summary.txt"
     #   Index the finished BAM file
     samtools index "${outDirectory}/${sampleName}.bam"
     #   Rename the index file
     mv "${outDirectory}/${sampleName}.bam.bai" "${outDirectory}/${sampleName}.bai"
+    #   Remove the unsorted BAM
+    rm "${outDirectory}/${sampleName}_unsorted.bam"
 }
 
 export -f NP_SAM_Processing
