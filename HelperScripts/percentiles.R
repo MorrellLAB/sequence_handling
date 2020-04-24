45f22ccb8e43ea8f561a9b80fa901ac47fae577b
ChaochihL <liux1299@umn.edu>
2020-04-24 15:29:17 -0500

    replace read.table with fread for faster loading of data

--- HelperScripts/percentiles.R
+++ HelperScripts/percentiles.R
@@ -3,6 +3,8 @@
 #   This script writes a percentiles table of the GQ and DP per sample from a VCF file
 #   Usage: Rscript percentiles.R <GQ matrix> <DP per sample matrix> <output_directory> <project_name> <filtered_status>
 
+library(data.table)
+
 #   A function to run the script
 runScript <- function() {
     options(warn=2)
@@ -15,12 +17,15 @@ runScript <- function() {
     status <- args[5]
     #   Make the GQ table
     GQ_path <- paste0(out, "/", project, "_", status, "_GQ.txt")
-    GQ <- read.table(file=GQ_matrix, sep="\t", na.strings=".")
+    #GQ <- read.table(file=GQ_matrix, sep="\t", na.strings=".")
+    GQ <- fread(file=GQ_matrix, sep="\t", na.strings=".") # Faster way to read in file
     GQ_quantile <- quantile(as.numeric(as.matrix(as.vector(GQ))),probs=seq(0,1,0.01),na.rm = TRUE)
     write.table(x=GQ_quantile,file=GQ_path,sep="\t", quote=FALSE, col.names=FALSE)
+
     #   Make the DP table
     DP_path <- paste0(out, "/", project, "_", status, "_DP_per_sample.txt")
-    DP <- read.table(file=DP_matrix, sep="\t", na.strings="NA")
+    #DP <- read.table(file=DP_matrix, sep="\t", na.strings="NA")
+    DP <- fread(file=DP_matrix, sep="\t", na.strings="NA") # Faster way to read in file
     DP_quantile <- quantile(as.numeric(as.matrix(as.vector(DP))),probs=seq(0,1,0.01),na.rm = TRUE)
     write.table(x=DP_quantile,file=DP_path,sep="\t", quote=FALSE, col.names=FALSE)
 }
