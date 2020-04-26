18937147e4da2ca26effe7731fef351001993145
ChaochihL <liux1299@umn.edu>
2020-04-26 14:54:40 -0500

    ff/ffbase still had problems, use bigmemory R package instead

--- HelperScripts/percentiles.R
+++ HelperScripts/percentiles.R
@@ -5,19 +5,20 @@
 
 #   If running this on MSI and there are problems with the installation above, please see here for
 #   instructions to install this package in your home directory: https://www.msi.umn.edu/sw/r
-.libPaths() # Check, remove after debugging
 #library(data.table)
-library(bit) # Required by ff
-library(ff)
-library(ffbase)
+#library(bit) # Required by ff
+#library(ff)
+#library(ffbase)
+library(bigmemory)
 
 #   A function to run the script
 runScript <- function() {
     options(warn=2)
     args <- commandArgs(trailingOnly = TRUE)
     #   Set the arguments
-    GQ_matrix <- args[1]
-    DP_matrix <- args[2]
+    # Use absolute filepath
+    GQ_matrix <- args[1] # Note: bigmemory will return error if "~" is in filepath
+    DP_matrix <- args[2] # Note: bigmemory will return error if "~" is in filepath
     out <- args[3]
     project <- args[4]
     status <- args[5]
@@ -25,9 +26,10 @@ runScript <- function() {
     GQ_path <- paste0(out, "/", project, "_", status, "_GQ.txt")
     #GQ <- read.table(file=GQ_matrix, sep="\t", na.strings=".")
     #GQ <- fread(file=GQ_matrix, sep="\t", na.strings=".") # Faster way to read in file
-    GQ <- read.table.ffdf(file=GQ_matrix, sep="\t", na.string=".") # Better handles large files
+    #GQ <- read.table.ffdf(file=GQ_matrix, sep="\t", na.string=".")
+    GQ <- read.big.matrix(GQ_matrix, sep="\t", type="integer") # Better handles large files
     #GQ_quantile <- quantile(as.numeric(as.matrix(as.vector(GQ))),probs=seq(0,1,0.01),na.rm = TRUE)
-    GQ_quantile <- quantile(GQ$V1, probs=seq(0,1,0.01))
+    GQ_quantile <- quantile(GQ[, 1], probs=seq(0,1,0.01))
     write.table(x=GQ_quantile, file=GQ_path, sep="\t", quote=FALSE, col.names=FALSE)
     # Remove GQ to free up memory
     rm(GQ, GQ_quantile)
@@ -36,9 +38,10 @@ runScript <- function() {
     DP_path <- paste0(out, "/", project, "_", status, "_DP_per_sample.txt")
     #DP <- read.table(file=DP_matrix, sep="\t", na.strings="NA")
     #DP <- fread(file=DP_matrix, sep="\t", na.strings="NA") # Faster way to read in file
-    DP <- read.table.ffdf(file=DP_matrix, sep="\t", na.strings="NA") # Better handles large files
+    #DP <- read.table.ffdf(file=DP_matrix, sep="\t", na.strings="NA")
+    DP <- read.big.matrix(file=DP_matrix, sep="\t", type="integer") # Better handles large files
     #DP_quantile <- quantile(as.numeric(as.matrix(as.vector(DP))),probs=seq(0,1,0.01),na.rm = TRUE)
-    DP_quantile <- quantile(DP$V1, probs=seq(0,1,0.01))
+    DP_quantile <- quantile(DP[, 1], probs=seq(0,1,0.01))
     write.table(x=DP_quantile, file=DP_path, sep="\t", quote=FALSE, col.names=FALSE)
 }
 
