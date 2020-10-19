009d5bb760ece88dd551fe6e44de15e214d4ac88
Emily Dittmar <Dittmare@gmail.com>
2020-10-19 17:56:44 -0400

    debug to see if printing 2 graphs is causing problems

--- HelperScripts/graph_annotations.R
+++ HelperScripts/graph_annotations.R
@@ -93,14 +93,12 @@ SNP_FS <- ggplot(Mydf$SNP, aes(x=FS + 1)) + geom_density(aes(fill=Cat), na.rm=TR
   xlab("Fisher Strand (log-scaled)")
 
 # Build output filepath
-outfile_snp_fp <- paste0(Directory, "/Percentile_Tables/SNP_distributions.png")
-SNP_plot <- arrangeGrob(SNP_plots$QD, SNP_FS, SNP_plots$SOR, SNP_plots$MQ, SNP_plots$MQRankSum, SNP_plots$ReadPosRankSum, 
-             SNP_plots$DP, SNP_plots$QUAL, nrow=4)
-ggsave(outfile_snp_fp,
-       plot = SNP_plot,
-       device = "png")
-
-print("Finished making SNP plot")
+#outfile_snp_fp <- paste0(Directory, "/Percentile_Tables/SNP_distributions.png")
+#SNP_plot <- arrangeGrob(SNP_plots$QD, SNP_FS, SNP_plots$SOR, SNP_plots$MQ, SNP_plots$MQRankSum, SNP_plots$ReadPosRankSum, 
+             #SNP_plots$DP, SNP_plots$QUAL, nrow=4)
+#ggsave(outfile_snp_fp,
+       #plot = SNP_plot,
+       #device = "png")
 
 
 #########################
@@ -126,7 +124,7 @@ print("Created FS plot for Indels")
 
 # Build output filepath
 outfile_indel_fp <- paste0(Directory, "/Percentile_Tables/INDEL_distributions.png")
-print(paste0("Output file will go", outfile_indel_fp))
+print(paste0("Output file will go in ", outfile_indel_fp))
 
 Indel_plot <- arrangeGrob(Indel_plots$QD, Indel_FS, Indel_plots$SOR, Indel_plots$MQ, Indel_plots$MQRankSum, Indel_plots$ReadPosRankSum, 
              Indel_plots$DP, Indel_plots$QUAL, nrow=4)
