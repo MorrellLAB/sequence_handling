df7ea3de68fea3add66874f198cf047edcb45173
ChaochihL <liux1299@umn.edu>
2020-11-22 20:11:36 -0600

    add option to reduce memory requirement

--- Handlers/Indel_Realigner.sh
+++ Handlers/Indel_Realigner.sh
@@ -24,6 +24,7 @@ function Indel_Realigner() {
     local entropy="$8" # What is the entropy threshold?
     local qscores="$9" # Do we fix quality scores?
     local gatkVer="${10}" # Either 3 or 4
+    local max_reads_in_mem="${11}"
     declare -a intervals_array=($(grep -E ".intervals" "${intervals_list}")) # Put the intervals list into array format
     local current_intervals="${intervals_array[${PBS_ARRAYID}]}" # Get the intervals file for the current sample
     declare -a sample_array=($(grep -E ".bam" "${sample_list}")) # Put the sample list into array format
@@ -33,27 +34,41 @@ function Indel_Realigner() {
     mkdir -p "${out}"
     #   Change into the output directory
     cd "${out}"
+    #   To prevent potential memory issues, the -Xmx value should be a little less than the total amount of physical memory
+    #       by at least a few GB.
+    #   So, we will subtract a few GB of mem from user provided memory
+    mem_num=$(basename ${memory} g)
+    # Check that we have requested the minimum required memory (>4g)
+    if [ ${mem_num} -le 4 ]; then
+        echo "You have 4g or less requested memory, please request more memory for this handler to work. Exiting..."
+        exit 31
+    fi
+    new_mem_num="$((${mem_num}-4))"
+    mem=$(printf "${new_mem_num}g")
+    #   Start indel realignment
     if [[ "${gatkVer}" == 3 ]]; then
         if [[ "${qscores}" == true ]]
         then
         #   Run GATK using the parameters given
-        java -Xmx"${memory}" -jar "${gatk}" \
+        java -Xmx"${mem}" -jar "${gatk}" \
             -T IndelRealigner \
             -R "${reference}" \
             --entropyThreshold "${entropy}" \
             --LODThresholdForCleaning "${lod}" \
             --targetIntervals "${current_intervals}" \
+            --maxReadsInMemory ${max_reads_in_mem} \
             -I "${current}" \
             --fix_misencoded_quality_scores \
             -o "${out}/${name}_realigned.bam"
         else
         #   Run GATK using the parameters given
-        java -Xmx"${memory}" -jar "${gatk}" \
+        java -Xmx"${mem}" -jar "${gatk}" \
             -T IndelRealigner \
             -R "${reference}" \
             --entropyThreshold "${entropy}" \
             --LODThresholdForCleaning "${lod}" \
             --targetIntervals "${current_intervals}" \
+            --maxReadsInMemory ${max_reads_in_mem} \
             -I "${current}" \
             -o "${out}/${name}_realigned.bam"
         fi
