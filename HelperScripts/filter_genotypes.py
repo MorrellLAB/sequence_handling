747cf7e6c16f2867489a70758adb81c548c4914b
Emily Loring Dittmar <eld72413@ss-sub2.gacrc.uga.edu>
2020-12-30 13:45:49 -0500

    Situation for cases of haploid genotype calls

--- HelperScripts/filter_genotypes.py
+++ HelperScripts/filter_genotypes.py
@@ -54,7 +54,7 @@ def filter_genotypes(f, mindp, maxdp, mindev, gt_cutoff, per_sample_coverage_cut
                         if gt == './.' or gt == '.|.':
                             continue  # note there are some cases with only two fields like './.:0,0'
                         if gt == '.':
-                            sys.stderr.write("FILTER GENOTYPES WARN: haplotype genotype ('"  + gt + "') encountered at: " +tmp[0]+ " "+tmp[1]+", "+"Genotype= #"+str(geno_index)+"\n")
+                            sys.stderr.write("FILTER GENOTYPES WARN: haploid genotype ('"  + gt + "') encountered at: " +tmp[0]+ " "+tmp[1]+", "+"Genotype= #"+str(geno_index)+"\n")
                             continue
                         dp = gt_metadata[2]
                         ad = gt_metadata[1].split(',')
